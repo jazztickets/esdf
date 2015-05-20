e6050f438ed1a87a04af24459e116dc671f659bd
Alan Witkowski <alan.witkowski@gmail.com>
2015-05-20 10:43:40 -0600

    fixed compile error

--- src/opengl.cpp
+++ src/opengl.cpp
@@ -16,6 +16,7 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *******************************************************************************/
 #include <opengl.h>
+#include <SDL_video.h>
 
 // Setup function pointers
 void InitGLFunctions() {
